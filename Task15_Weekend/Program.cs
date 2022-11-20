7d9efe89b630287e5156c52ede609cbed97a3dcb
M_Sova <MyMail@mail.ru>
2022-11-21 01:47:30 +0300

    task 15

---
+++ Task15_Weekend/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write ("Введите цифру дня недели: ");
+int date = Convert.ToInt32 (Console.ReadLine());
+if (date == 6 || date == 7)
+{
+    Console.Write ("Выходной день");
+}
+else if (date < 1 || date > 7)
+{
+    Console.Write ("Вы ввели несуществующий день недели");
+}
+else
+{
+    Console.Write ("Будний день");
+}
